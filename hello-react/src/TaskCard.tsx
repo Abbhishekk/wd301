f2dadd5b599fce4abce55a724c90655b519ef21a
Abhishek Bante <abhishek.bante.cse@ghrce.raisoni.net>
2023-08-08 19:57:38 +0530

    Updated due on and completed At

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -5,8 +5,11 @@ const TaskCard = (props)=>{
       <div className="border-4 border-slate-400 p-2 m-10">
         <h2>{props.title}</h2>
         
-            <p>{props.task}: {props.dueDate}{props.completedAtDate}</p>
-        
+            {props.dueDate ? (
+            <p>Due on: {props.dueDate}</p>
+          ) : (
+            <p>Completed on: {props.completedAtDate}</p>
+          )}
         <p>Assignee: {props.assigneeName}</p>
         
       </div>
