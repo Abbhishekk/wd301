75f28330ccfc8bca07282028d51d12f927605867
Abhishek Bante <abhishek.bante.cse@ghrce.raisoni.net>
2023-08-25 19:21:07 +0530

    create event handlers

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import TaskCard from './TaskCard'
-import "./TaskCard.css"
-import React from 'react'
+import React from 'react';
+import './index.css';
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
+import TaskApp from "./TaskApp";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <div className=' grid justify-center mt-4 ml-20 mr-20 font-sans '>
-        <h2 className='font-bold text-2xl'> Smarter Tasks</h2>
-        <h4 className=''> <span className='font-bold text-slate-400'> Project: </span> Graduation Final Year Project Revamp College Website</h4>
-        <div className='flex justify-center gap-10 p-10'>
-        <div className=' rounded-2xl border-4 w-96 '>
-          <h1 className='text-xl text-center mt-4 font-bold text-slate-500'>Pending</h1>
-          <TaskCard title="Build website" dueDate="10th April" assigneeName="Rohit"  />
-          <TaskCard title="Build website" dueDate="10th April" assigneeName="Rohit" />
-          <div className='bg-slate-200  m-10'>+ New Task</div>
-        </div>
-        <div className=' rounded-2xl border-4 w-96 '>
-        <h1 className='text-xl text-center mt-4  font-bold text-slate-500'>Done</h1>
-        <TaskCard title="Build website" completedAtDate="10th April" assigneeName="Rohit"  />
-          <TaskCard title="Build website" completedAtDate="10th April" assigneeName="Rohit" />
-        </div>
-      </div>
-      </div>
-  )
+    <div className="App">
+      <TaskApp />
+    </div>
+  );
 }
-
-
-
-
-export default App
+export default App;
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,19 +1,17 @@
-import React from "react"
-import "./TaskCard.css"
-const TaskCard = (props)=>{
-    return (
-      <div className="border-4 border-slate-400 p-2 m-10">
-        <h2>{props.title}</h2>
-        
-            {props.dueDate ? (
-            <p>Due on: {props.dueDate}</p>
-          ) : (
-            <p>Completed on: {props.completedAtDate}</p>
-          )}
-        <p>Assignee: {props.assigneeName}</p>
-        
-      </div>
-    )
-  }
+import React from "react";
 
-  export default TaskCard
+const TaskCard = ({ title, dueDate, completedAtDate, assigneeName }) => {
+  return (
+    <div className="border-2 border-slate-300 rounded-xs p-2 mb-2">
+      <h3 className="font-bold text-slate-600 mb-2">{title}</h3>
+      {dueDate ? (
+        <p>Due on: {dueDate}</p>
+      ) : (
+        <p>Completed on: {completedAtDate}</p>
+      )}
+      <p>Assignee: {assigneeName}</p>
+    </div>
+  );
+};
+
+export default TaskCard;
